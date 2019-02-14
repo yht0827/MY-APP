138a80c5897e12d6980234e4f7d6bf3923c977c7
yht0827 <yangheetae0827@naver.com>
2019-02-14 12:23:46 +0900

    no.4

--- src/Movie.js
+++ src/Movie.js
@@ -10,18 +10,14 @@ class Movie extends Component {
             </div>
             );
         }
-
 }
 
 class MoviePoster extends Component{
     render(){
         return(
-            <img src={this.props.poster}/>           
+            <img src={this.props.poster} alt=""/>           
         );
     }
 }
-
-
-
 
 export default Movie;
