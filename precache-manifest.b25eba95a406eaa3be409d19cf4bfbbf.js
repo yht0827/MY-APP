f0fefb4b67ee349b713cc9a7fb1fd48e4dd76b62
yht0827 <yangheetae0827@naver.com>
2019-02-16 01:04:43 +0900

    Updates

---
+++ precache-manifest.b25eba95a406eaa3be409d19cf4bfbbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5607f6b57af79a0916bb",
+    "url": "/MY-APP/static/js/runtime~main.5607f6b5.js"
+  },
+  {
+    "revision": "eb8b0d1b33335e51a366",
+    "url": "/MY-APP/static/js/main.eb8b0d1b.chunk.js"
+  },
+  {
+    "revision": "3d59134098572823c6c8",
+    "url": "/MY-APP/static/js/2.3d591340.chunk.js"
+  },
+  {
+    "revision": "eb8b0d1b33335e51a366",
+    "url": "/MY-APP/static/css/main.d6d0d918.chunk.css"
+  },
+  {
+    "revision": "74f572e89a4af58ddd8f8788c836cb22",
+    "url": "/MY-APP/index.html"
+  }
+];
